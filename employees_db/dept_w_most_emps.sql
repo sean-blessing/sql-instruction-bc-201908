8a83ddcd3cf73dbc1a11615f07618e3195871665
Sean Blessing <snblessing@gmail.com>
2019-09-12 22:06:55 -0400

    added dept w most emps query

---
+++ employees_db/dept_w_most_emps.sql
@@ -0,0 +1,10 @@
+select *
+  from dept_emp;
+
+select d.productdept_no, dept_name, count(*) total
+  from dept_emp de
+  join departments d
+    on de.dept_no = d.dept_no
+ where to_date = '9999-01-01'
+ group by dept_no
+ order by total desc;
